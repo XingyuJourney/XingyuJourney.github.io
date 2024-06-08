652bc0d257a24b647f00de424772df469b1fa4dd
XingyuJourney <funny_wade@outlook.com>
2024-06-08 03:55:08 +0100

    update site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First time to Highland",
+        "excerpt":"First time to Scottish Highlands Street of Inverness The northernmost city in the UK Loch Ness I didn’t see the Loch Ness Monster, but the scenery by the lake was nice. I spent the entire day before I leave by sit near the Loch and surprise the fog is come,...","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/01/09/HighlandTime1.html",
+        "teaser": null
+      }]
