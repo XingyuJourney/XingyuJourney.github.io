a0c793acd36515562b770a04b8cfaa7bf0d86d7c
XingyuJourney <funny_wade@outlook.com>
2024-08-26 14:06:44 +0800

    Update site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,103 @@
+var store = [{
+        "title": "Scottish Highlands Part 1",
+        "excerpt":"Scottish Highlands Inverness The northernmost city in the UK Loch Ness I didn’t see the Loch Ness Monster, but the scenery by the lake was nice. I spent the entire day before I leave by sit near the Loch and surprise the fog is come, which make the Loch Ness...","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/01/09/HighlandTime1.html",
+        "teaser": null
+      },{
+        "title": "London",
+        "excerpt":"London Big Ben A landmark building in London The National Gallery of UK I browsed the original works of Van Gogh here，who is one of my favourite artists. Van Gogh’s tragic life is reflected in the emotional depth of his work, also he had a profound understanding of color usage....","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/01/09/London.html",
+        "teaser": null
+      },{
+        "title": "Ilkeston",
+        "excerpt":"Ilkeston   Bennerley Viaduct  An old bridge in Ilkeston. Completed in 1877, it carries the Derbyshire branch of the Great Northern Railway and crosses the River Erewash and its broad, flat valley which forms the county boundary.                    The windmill and surrounding farms can be seen from the Bennerley Viaduct                   ","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/01/27/IIkeston.html",
+        "teaser": null
+      },{
+        "title": "River Trent",
+        "excerpt":"River Trent   A river that flows through Nottingham. I love walking along the river.   Sunset in river trent during the dry season           Sunset in river trent during the wet season             ","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/01/27/NearNottingham.html",
+        "teaser": null
+      },{
+        "title": "Peak District Part 1",
+        "excerpt":"Edensor   St Peter’s Church  Classic and beautiful building           Views around Edensor  Edensor is surrounded by large pastures and lawns, perfect for a leisurely afternoon walk.                             A peaceful house.           During my walking, I came across a tree standing alone in the wilderness.             ","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/02/12/PeakDistrictTime1.html",
+        "teaser": null
+      },{
+        "title": "Peak District Part 2",
+        "excerpt":"Hope Valley Win Hill The name is pretty interesting. A tree I came across at the beginning of climb. The weather was not good during the climbing, even hail in the middle. Luckily, the sun came out after we reached the top of the hill. At this time, I can...","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/02/23/PeakDistrictTime2.html",
+        "teaser": null
+      },{
+        "title": "Peak District Part 3",
+        "excerpt":"Chesterfield   Parish Church  Church with spiral angle           Baslow   Gardom’s Edge  Nice looking stone piles and a relaxing trail                    Eagle Stone  A giant stone             ","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/03/04/PeakDistrictTime3.html",
+        "teaser": null
+      },{
+        "title": "Peak District Part 4",
+        "excerpt":"Baslow  Baslow Edge  The huge edge that can be seen from a distance, which is very impressive when you walk near.                             The light  While taking a short break on the edge, I saw a light in the distance breaking through the clouds.             ","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/03/04/PeakDistrictTime4.html",
+        "teaser": null
+      },{
+        "title": "Scottish Highlands Part 2",
+        "excerpt":"Scottish Highlands Loch Linnhe A loch located on the west coast of Scotland. Loch Linnhe is part of the Grand Canyon and the only lake in the Grand Canyon that is actually part of the ocean. It is rumored that an unknown marine creature called “Linnhe” lives here. (Why does...","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/03/27/HighlandTime2.html",
+        "teaser": null
+      },{
+        "title": "Scottish Highlands Part 3 (Ben Nevis)",
+        "excerpt":"Ben Nevis Conclusion: In harsh weather, it is unwise to climb the snowy mountain alone. If your equipment is inadequate and time is short, it is best to avoid the journey altogether. Stage zero At first I didn’t realize the seriousness of this situation. Stage one As I started to...","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/03/27/HighlandTime3.html",
+        "teaser": null
+      },{
+        "title": "Scottish Highlands Part 4",
+        "excerpt":"Glen Nevis The glen is bordered by the Marmore Mountains to the south and the highest mountains in the British Isles to the north: Ben Nevis, Càrn Mor Dearg, Aonach Mòr, and Aonach Beag. Walking through it, you can admire the unique landscape left on the mountain by glacial erosion....","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/03/27/HighlandTime4.html",
+        "teaser": null
+      },{
+        "title": "Shanghai",
+        "excerpt":"Shanghai   A big city in China                      ","categories": ["blog"],
+        "tags": ["China"],
+        "url": "/blog/2024/04/22/Shanghai.html",
+        "teaser": null
+      },{
+        "title": "UoN Jubilee Campus",
+        "excerpt":"University of Nottingham   Jubilee Campus  The place I study in, a nice place.                               ","categories": ["blog"],
+        "tags": ["China"],
+        "url": "/blog/2024/04/22/UoNJubileeCampus.html",
+        "teaser": null
+      },{
+        "title": "Peak District Part 5",
+        "excerpt":"Chatsworth House  A grand stately home                    Edensor  Front           On the road to Edensor from Bakewell, I take two pictures.           This tree again.             ","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/05/24/PeakDistrictTime5.html",
+        "teaser": null
+      },{
+        "title": "Peak District Part 6",
+        "excerpt":"Baslow  I came to Baslow again, but this time I lay on the rocks, bathed in the sun all day and only took a few pictures.                                ","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/06/02/PeakDistrictTime6.html",
+        "teaser": null
+      },{
+        "title": "Peak District Part 7",
+        "excerpt":"Kinder Scout I made an appointment with my friend to go hiking together. It rained heavily before we started climbing, but fortunately the weather was good when we actually started climbing. Because we didn’t look at the map when climbing, we realised we climbed the wrong mountain after we reached...","categories": ["blog"],
+        "tags": ["UK"],
+        "url": "/blog/2024/06/06/PeakDistrictTime7.html",
+        "teaser": null
+      },{
+        "title": "Okutama",
+        "excerpt":"Okutama   The mountains near Tokyo look beautiful from the outside, but it’s not comfortable to walk in. The trees are so dense that you can’t see the sun, but the temperature is cool.                         A beautiful house among mountains              ","categories": ["blog"],
+        "tags": ["Japan"],
+        "url": "/blog/2024/07/06/Okutama.html",
+        "teaser": null
+      }]
